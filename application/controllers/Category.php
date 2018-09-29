ad48d8aec710b5e0e1dff51ff306867f7a192b87
p4kuc1n9 <agit@qwords.co.id>
2018-09-29 15:47:38 +0700

    Categori & Category Detail

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -5,26 +5,47 @@
         function _construct(){
             parent::__construct();
         }
-
         public function index(){
-            
+            $this->libCategory();
         }
         public function loadCategory(){
-            $data = $this->model_category->all()->row_object();
-            if($data==null){
-                makeOut(99,null);
-            }else{
-                makeOut(1000,$data);
+            $data = $this->libCategory();
+            foreach($data as $row){
+                unset($row['path']);
+                $var[]=$row;
             }
+            makeOut(1000,$var);
         }
 
         public function loadCategoryDetail($id=''){
-            $data = $this->model_category->get_field('id',$id)->row_object();
-            if($data==null){
-                makeOut(99,null);
-            }else{
-                makeOut(1000,$data);
-            }
+           $data = $this->libCategory();
+           foreach($data as $d){
+               $cat[]=$d['id'];
+           }
+           if(in_array($id,$cat)){
+               $fopen = fopen($data[$id]['path'],'r');
+               $read  = fread($fopen,filesize($data[$id]['path']));
+               $decode = json_decode($read);
+               makeOut(1000,$decode->data);
+           }else{
+               makeOut(99,'Categori Not Found');
+           }
         }
+        private  function libCategory(){
+            $base    = "/var/www/html/kopipasteu/log/";
+            $data    = shell_exec("ls  $base*.json");
+            $explode = array_filter(explode("\n",$data));
+            $var     = null;
+            foreach($explode as $e){
+                $filename = substr($e,strlen($base),strlen($e));
+                $code     = substr($e,strlen($base),strlen($filename)-5);
+                $var [$code]= [
+                    'id'   => $code,
+                    'name' => str_replace("_"," ",substr($e,strlen($base),strlen($filename)-5)),
+                    'path' => $e,
+                ];
+            }
+            return $var;
+        }  
 
     }
--- application/controllers/Maps.php
+++ application/controllers/Maps.php
@@ -11,4 +11,26 @@
             $fread = fread($fopen,filesize($file));  
             makeOut(1000,json_decode($fread));
         }
+        private function  _read_data(){
+            
+            $file = './log/maps.json';
+            $fopen = fopen($file,'r');
+            $fread = fread($fopen,filesize($file));
+            $response=  json_decode($fread);  
+            return $response->data;
+        }
+        function download(){
+            $data = $this->_read_data();
+            foreach($data as $d){
+                $sc[]= "wget $d->tenant_logo";
+            }
+            $var = implode("\n",$sc);
+
+            $fopen = fopen("./log/imgmaps/download.sh",'w+');
+            
+            fwrite($fopen,$var);
+
+            makeOut(1000,'Success Download');
+        }
+
     }
