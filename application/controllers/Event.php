34807ebcc271d63b52646cfee44794d651c5086d
p4kuc1n9 <agit@qwords.co.id>
2018-10-06 18:03:02 +0700

    Event -001

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -8,11 +8,12 @@
         
         function getEvent()
         {
-            $filename = '/home/agitnaeta/public_html/kopipasteu/log/event/';
+            $filename = '/home/agitnaeta/public_html/kopipasteu/log/event/event.json';
             // $filename = '/var/www/html/kopipasteu/log/event/event.json';
             
             if(!@fopen($filename,'r')){
                makeOut(99,'Error Load File');
+               die;
             }
             $fopen = fopen($filename,'r');
             $fread = json_decode(fread($fopen,filesize($filename)));
